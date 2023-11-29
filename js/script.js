b467a598f98037dbc491f60a1737bdd37e2b3052
Ahmad Ubai Dullah <ahm.idlah773@gmail.com>
2023-11-29 12:54:37 +0700

    init_html

--- js/script.js
+++ js/script.js
@@ -1,16 +1,143 @@
 
-// ganti warna
+//Function sign up
 
-  const btn = document.querySelector('.btv');
-  console.log(btn);
+// Inisialisasi variabel
+let firstNameInput = document.getElementById("first-name");
+let lastNameInput = document.getElementById("last-name");
+let emailInput = document.getElementById("email");
+let passwordInput = document.getElementById("password");
 
-  function random(number) {
-    return Math.floor(Math.random() * (number+1));
+// Event listener untuk tombol "Sign Up"
+form.addEventListener("submit", (event) => {
+  // Validasi input
+  if (firstNameInput.value === "") {
+    alert("Nama depan tidak boleh kosong.");
+    event.preventDefault();
+    return;
   }
 
-  btn.addEventListener('click', () => {
-    const rndCol = `rgb(${random(500)}, ${random(500)}, ${random(500)})`;
-    document.body.style.backgroundColor = rndCol;
-  });
+  // ...
 
+//   // Kirim data ke server
+//   // (Kode ini perlu disesuaikan dengan kebutuhan Anda)
+//   fetch("api/signup", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       firstName: firstNameInput.value,
+//       lastName: lastNameInput.value,
+//       email: emailInput.value,
+//       password: passwordInput.value,
+//     }),
+//   })
+//     .then((response) => {
+//       if (response.ok) {
+//         // Sukses
+//         alert("Pendaftaran berhasil.");
+
+//         // Reset input
+//         firstNameInput.value = "";
+//         lastNameInput.value = "";
+//         emailInput.value = "";
+//         passwordInput.value = "";
+//       } else {
+//         // Gagal
+//         alert(response.statusText);
+//       }
+//     })
+//     .catch((error) => {
+//       // Error
+//       console.error(error);
+//     });
+// });
+
+
+// function khusus login 
+function validateForm() {
+    // Validasi username
+    var username = document.getElementById("username").value;
+    if (username === "") {
+      alert("Username tidak boleh kosong");
+      return false;
+    }
   
+    // Validasi password
+    var password = document.getElementById("password").value;
+    if (password === "") {
+      alert("Password tidak boleh kosong");
+      return false;
+    }
+  
+    // Validasi panjang password
+    if (password.length < 6) {
+      alert("Password harus minimal 6 karakter");
+      return false;
+    }
+  
+    // Validasi format email
+    var email = document.getElementById("email").value;
+    if (!/^\w+@\w+\.\w+$/.test(email)) {
+      alert("Email tidak valid");
+      return false;
+    }
+  
+    return true;
+  }
+  
+  function checkLogin() {
+    // Dapatkan username dan password dari form
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+  
+    // Kirim username dan password ke server
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/api/login", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify({
+      username: username,
+      password: password,
+    }));
+  
+    // Tunggu respons dari server
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        alert("Selamat anda Berhasil Login")
+        window.location.href = "/";
+      } else {
+        // Login gagal
+        alert("Login gagal");
+      }
+    };
+  }
+  
+  function loadForm() {
+    // Dapatkan elemen form
+    var form = document.getElementById("form-login");
+  
+    // Tambahkan efek animasi
+    form.classList.add("animated", "fadeIn");
+  }
+  
+  // Event handler
+  document.getElementById("form-login").onsubmit = validateForm;
+  
+  // Tampilan form login
+  loadForm();
+
+
+
+
+
+ const btn = document.querySelector('.btv');
+         
+          function random(number) {
+              return Math.floor(Math.random() * (number+1));
+            }
+         
+            btn.addEventListener('click', () => {
+              const rndCol = `rgb(${random(500)}, ${random(500)}, ${random(500)})`;
+              document.body.style.backgroundColor = rndCol;
+            });
+ 
